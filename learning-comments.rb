9bbebbb4873f4c1ff668dea200c22d3c13deba00
Logan McGuire <lrmcguire93@gmail.com>
2021-04-16 23:42:22 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,18 @@
+# Don't do this: 
+# puts ( 1 #+1);
+
+#Don't just state what code is doing
+#Reference code is useful aka artist and song name for 'puts' lyrics
+#This is an in-code documentation convention 
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world";
+puts "All the boys and girls";
+#puts "Joy to the fishes in the deep blue sea";
+puts "Joy to you and me";
+
+#name = "Byron";
+name = "Luca";
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+
---
+++ nested_arrays_addendum.rb
@@ -0,0 +1,17 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+
+p spice_rack;
+
+spice_rack[1][1] = "Extract of Pizza";
+
+p spice_rack;
+
+spice_rack[1][100] = "Poodle Dinner";
+
+p spice_rack;
+
+#adds a whole lotta nil 
